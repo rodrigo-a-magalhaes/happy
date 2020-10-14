65a1c0959dfea66bb240aca1866fad2b544d1860
Rodrigo Almeida de Magalhaes <rodrigo.a.magalhaes@hotmail.com>
2020-10-14 01:33:15 -0300

    primeira aula

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages.css';
+
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Minas Gerais</strong>
+          <span>Uberlândia</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-18.9121206, -48.2642293]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFFFFF" />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
